9f7b7e01bd9fc06a8941edb307c67134637f3bde
Mustafa Asaad <ma24th@yahoo.com>
2022-03-30 16:32:13 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ddbotapi',
+    version='9.0.0',
+    packages=['ddbotapi'],
+    url='https://github.com/MA24th/ddbotapi',
+    license='GPLv2',
+    author='Mustafa Asaad',
+    author_email='ma24th@outlook.com',
+    description='Discord Bot API Framework'
+)
