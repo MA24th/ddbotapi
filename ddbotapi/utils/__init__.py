403e8ffdff5169537d8b6f3b403bef31ea0287e1
Mustafa Asaad <ma24th@yahoo.com>
2022-03-30 17:39:06 +0300

    Add utils as package

---
+++ ddbotapi/utils/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+ddbotapi.utils
+~~~~~~~~~~~~~~
+This submodule provides utility functions that are consumed internally
+:copyright: (c) 2022 by Mustafa Asaad.
+:license: GPLv2, see LICENSE for more details.
+"""
+
+from .json_helper import *
